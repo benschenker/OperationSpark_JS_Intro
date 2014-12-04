871621b2f13f29fc05c91f85bc332114754a4d86
Ben Schenker <binsch@gmail.com>
2014-12-04 04:47:41 +0000

    Add Lab 2

---
+++ lab2.js
@@ -0,0 +1,11 @@
+// 1. Write one variable that is named "degreesInCelsius" and is instantiated to the number 35.
+
+
+
+// 2. Write another variable that is named "degreesInFahrenheight" and is instantiated to the formula used to convert Celsius to Fahrenheit. 
+//      Use the variable defined in (1) as in the input.
+
+
+
+// 3. Print the value of the variable created in (2).
+
